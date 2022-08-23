673235d7e755243026c89bc42fc4f9c26b0fd69a
guidopilato <guidopilato@hotmail.com>
2022-08-23 00:26:30 -0300

    Agregado de Boton de logueo con bootstrap y Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //le digo que aplique el ruteo y que vaya 
+  //directamente al homecomponent
+  {path:'', component: HomeComponent}, 
+  {path:'login', component: LoginComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
