3aa53e0d5266a7d24f6cd3a907a26e5acea2a022
guidopilato <guidopilato@hotmail.com>
2022-08-29 02:50:24 -0300

    Modificaciones para Heroku y Firebase

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/';
+  URL = 'https://backendguidopilato.herokuapp.com/educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
